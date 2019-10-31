2a0fcfd3fa4e2b34bc7c2557d2c21d6b9ad94abc
memcmahon <mcmahon.meganelizabeth@gmail.com>
2019-10-31 17:24:00 -0600

    Walkthrough

--- lib/curator.rb
+++ lib/curator.rb
@@ -26,4 +26,36 @@ class Curator
       photo.id == id
     end
   end
+
+  def find_photographs_by_artist(artist)
+    @photographs.find_all do |photo|
+      photo.artist_id == artist.id
+    end
+  end
+
+  def artists_with_multiple_photographs
+    # if I had an artist object, I could call ^ with that artist as an arg and count the number of photos
+    # prolific_artists = []
+    @artists.find_all do |artist|
+      find_photographs_by_artist(artist).length > 1
+    end
+    # prolific_artists
+  end
+
+  def photographs_taken_by_artist_from(country)
+    # all artists, from 'country'
+
+    photos_from_country = []
+    artists_from(country).each do |artist|
+      photos_from_country << find_photographs_by_artist(artist)
+    end
+    photos_from_country.flatten
+    # all photos from those artists
+  end
+
+  def artists_from(country)
+    @artists.find_all do |artist|
+      artist.country == country
+    end
+  end
 end
